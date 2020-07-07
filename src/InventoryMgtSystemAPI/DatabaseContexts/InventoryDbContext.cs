9b31f68975efc9cf93f0d1eecfbfa8ff1e4b6a1c
Gabriel Isaac <iamkemical1@gmail.com>
2020-07-07 18:35:25 +0100

    Added connection string  and updated the startup class

--- src/InventoryMgtSystemAPI/DatabaseContexts/InventoryDbContext.cs
+++ src/InventoryMgtSystemAPI/DatabaseContexts/InventoryDbContext.cs
@@ -1,7 +1,15 @@
+using InventoryMgtSystemAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace InventoryMgtSystemAPI.DatabaseContexts
 {
-    public class InventoryDbContext
+    public class InventoryDbContext : DbContext
     {
-        
+        public InventoryDbContext(DbContextOptions<InventoryDbContext> options)
+        :base(options)
+        {
+            
+        }
+        public DbSet<InventoryModel> InventoryModel { get; set; }
     }
 }
