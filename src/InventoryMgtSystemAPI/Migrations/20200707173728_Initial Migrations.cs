d833617a11309a266c86eae7f8521dbf29a3b541
Gabriel Isaac <iamkemical1@gmail.com>
2020-07-08 01:28:17 +0100

    Added migrations and updated database

---
+++ src/InventoryMgtSystemAPI/Migrations/20200707173728_Initial Migrations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InventoryMgtSystemAPI.Migrations
+{
+    public partial class InitialMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "InventoryModel",
+                columns: table => new
+                {
+                    InventoryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ItemName = table.Column<string>(nullable: false),
+                    DatePurchased = table.Column<DateTime>(nullable: false),
+                    AmountPerItem = table.Column<int>(nullable: false),
+                    StockQty = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InventoryModel", x => x.InventoryId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InventoryModel");
+        }
+    }
+}
